b0925102128d80a2c7dc89b774254972150cc649
Suyahman <yahman@students.unnes.ac.id>
2021-09-03 14:22:19 +0700

    Update SiteMetadata.js

--- src/components/SiteMetadata.js
+++ src/components/SiteMetadata.js
@@ -8,7 +8,7 @@ const useSiteMetadata = () => {
           siteMetadata {
             title
             description
-            google-site-verification
+            google
           }
         }
       }
